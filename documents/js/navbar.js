bb9d10aecbb4840166ed446c64a07492d4483715
Hamza Shaikh <51785892+shaikhhamza11@users.noreply.github.com>
2022-02-02 19:15:03 +0530

    feat : revamp documentation page and added utilities classes

--- documents/js/navbar.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.querySelector('#toggleButton')
-const navBarLinks = document.querySelector(".navbar-links-container")
-
-// Navbar Toggle for mobile screen
-toggleButton.addEventListener('click',()=>{
-    navBarLinks.classList.toggle('active')
-})
